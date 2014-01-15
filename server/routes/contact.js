17042b8335f0d0cdce85c57678590f5dcd45065f
Breana Jones <breanajonesm@gmail.com>
2014-01-15 12:53:58 -0700

    Added Dropdown buttons on Navigation Bar: Edit Account Info, View Products, View Contacts

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,7 +1,5 @@
 module.exports = function(app, auth, contact) {
     app.get('/api/contacts', auth, contact.list);
-//    app.get('/api/contact/:id', auth, contact.contact);
     app.post('/api/contact', auth, contact.addContact);
-//    app.put('/api/contact/:id', contact.editPost);
-//    app.delete('/api/post/:id', contact.deletePost);
+    app.delete('/api/contact/:id', auth, contact.deleteContact);
 }
